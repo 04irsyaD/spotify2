abfb3ff3daee330573e9ac5371ece348a956166f
04irsyaD <irsyadrisno@gmail.com>
2022-08-07 22:39:41 +0700

    spotipi

---
+++ spoti2/js/scriptmusic.js
@@ -0,0 +1,21 @@
+let allmusic=[
+    {
+        name: "RAISE MY SWORD",
+        artist: "GALYNERUS",
+        img: "download",
+        src: "GALNERYUS  RAISE MY SWORDOFFICIAL MUSIC VIDEO"
+    },
+    {
+        name: "After Dark",
+        artist: "MrKitty",
+        img: "723893379034c16a7ed5a35d318bd8a1",
+        src: "MrKitty  After Dark"
+    },
+    {
+        name:"Dusk Till Dawn ft. Sia",
+        artist: "ZAYN FT SIA",
+        img: "Screenshot 2022-08-07 223213",
+        src: "ZAYN - Dusk Till Dawn ft. Sia"
+    }
+
+]
